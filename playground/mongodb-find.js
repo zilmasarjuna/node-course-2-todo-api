d2d7fb3714bca38541ac9130d84ea1cf8a8069fc
zilmasarjuna <zilmasarjuna@yahoo.com>
2018-11-13 17:19:08 +0700

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5be9440412c7d112ccae11b7')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // })
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // })
+
+  db.collection('User').find({name: 'Andrew'}).toArray().then((docs) => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  })
+
+  // client.close();
+});
